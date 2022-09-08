6f95c537e31c450884b926a39d034f3d45227603
Johannes Enevoldsen <johannes.ne@gmail.com>
2022-09-08 14:57:15 +0200

    simplify GAM plot

--- R/GAM_CVP.R
+++ R/GAM_CVP.R
@@ -3,6 +3,10 @@ source("R/common.R")
 update_geom_defaults("point",   list(colour = darkcolor))
 update_geom_defaults("line",   list(colour = darkcolor))
 
+theme_set(theme_thesis() %+replace% 
+            theme(plot.title = element_text(size = rel(1), hjust = 0, margin = margin(b = 5)),
+                       plot.subtitle = element_text(size = rel(0.8), hjust = 0)))
+
 library(waveformtools)
 
 cvp_viz_sample <- readRDS("sample_data/sample_cvp_insp_qrs_6min.RDS")
@@ -68,25 +72,10 @@ cvp_plot <- ggplot(cvp_demo_60 %>% first_20_s(), aes(time_s, CVP, group = 1)) +
   geom_point(aes(time_s, y = 18, shape = 'P wave (ECG)'), 
              data = p_demo_60 %>% first_20_s()) +
   scale_colour_discrete() +
-  labs(title = "**a** - Observed CVP",
-       subtitle = "The model is fitted on a 60 second recording. The first 20 seconds are shown here.",
+  labs(title = "Observed CVP [mmHg]",
        x = 'Time [s]',
-       y = ' CVP [mmHg]') +
-  theme(legend.position = 'bottom')
-#     
-# ecg_plot <- ggplot(ECG_demo_60, aes(time_s, ECG_II)) +
-#     geom_line() + 
-#     geom_point(aes(time_s, y = 1.2, shape = 'QRS complex'), data = qrs_demo_60, show.legend = FALSE) +
-#     ggtitle('Signals used for additional features (position in cardiac and respiratory cycle)',
-#             subtitle = 'ECG') +
-#     labs(y = 'ECG') +
-#     theme(axis.title.y = element_blank(),
-#           axis.text.y = element_blank(),
-#           axis.title.x = element_blank(),
-#           axis.text.x = element_blank(),
-#           axis.line.x = element_blank())
-
-# Model visualizations
+       y = ' CVP') +
+  theme(legend.position = c(0.8, 1.3), legend.direction = "horizontal")
 
 # Get representations of smooths
 cardiac_smooth <- gratia::smooth_estimates(gam_cvp_demo_60, 's(p_index)')
@@ -98,12 +87,8 @@ time_smooth <- gratia::smooth_estimates(gam_cvp_demo_60, 's(time_s)') %>%
 
 interact_smooth <- gratia::smooth_estimates(gam_cvp_demo_60, 'ti(p_index,insp_rel_index)', dist=0.3)
 
-y_scale_smooths <- scale_y_continuous(limits = c(-4, 5), expand = c(0,0))
+y_scale_smooths <- scale_y_continuous(limits = c(-4, 4), expand = c(0.05,0))
 
-scale_linetype_ventstate <- scale_linetype_manual(values = c("52","21", "84"),
-                                                  breaks = c("End expiration",
-                                                             "End inspiration",
-                                                             "Mid expiration")) 
 
 cvp_landmarks <- tribble(
   ~p_index, ~est, ~label,
@@ -115,30 +100,28 @@ cvp_landmarks <- tribble(
 )
 
 cardiac_smooth_plot <- ggplot(cardiac_smooth, aes(p_index, est)) +
-  geom_ci_ribbon +
   geom_line() +
   # Label CVP landmarks
-  geom_text(aes(label = label), 
-            color = "black",
-            size = 3,
-            nudge_y = 0.4,
-            data = cvp_landmarks, show.legend = FALSE) +  
-  geom_point(aes(x = 0, y = 4.2, shape = 'P wave (ECG)'), data = data.frame(), show.legend = FALSE) +
-  labs(x = 'Time since P wave [seconds]',
-       y = 'Partial CVP [mmHg]') +
-  ggtitle('**b** - Position in cardiac cycle') +
+  # geom_text(aes(label = label), 
+  #           color = "black",
+  #           size = 3,
+  #           nudge_y = 0.4,
+  #           data = cvp_landmarks, show.legend = FALSE) +  
+  geom_point(aes(x = 0, y = 3.5, shape = 'P wave (ECG)'), data = data.frame(), show.legend = FALSE) +
+  labs(x = 'Time since P wave [s]',
+       y = 'Partial CVP') +
+  ggtitle('Position in cardiac cycle') +
   #xlim(c(0,1.1)) +
   y_scale_smooths +
   coord_cartesian(clip = "off")
 
 insp_smooth_plot <- ggplot(insp_smooth, aes(insp_rel_index, est)) +
-  geom_ci_ribbon +
   geom_line() +
   geom_point(aes(x = 1, y = -1, shape = 'Inspiration start'), data = data.frame(), show.legend = FALSE) +
   geom_point(aes(x = 0, y = -1, shape = 'Inspiration start'), data = data.frame(), show.legend = FALSE) +
   labs(x = 'Respiratory cycle',
-       y = 'Partial CVP [mmHg]') +
-  ggtitle('**c** - Position in respiratory cycle') +
+       y = 'Partial CVP') +
+  ggtitle('Position in respiratory cycle') +
   scale_x_continuous(labels = scales::percent) +
   y_scale_smooths
 
@@ -146,30 +129,27 @@ interact_smooth_plot <- ggplot(interact_smooth, aes(p_index, insp_rel_index, z =
   #geom_contour_filled(show.legend = FALSE, binwidth = 0.2) +
   geom_raster(aes(fill = est), show.legend = FALSE) +
   geom_contour(colour = 'black', binwidth = 0.2) +
-  metR::geom_label_contour(skip = 1, size = 2.5, label.size = 0, 
+  metR::geom_label_contour(skip = 1, size = 2.5, label.size = 0,
                            #label.placement = metR::label_placement_fraction(frac = 0.1),
-                           label.padding = unit(0.01, "lines"), binwidth = 0.2) +
+                           label.padding = unit(0.01, "lines"), binwidth = 0.4) +
   #metR::geom_text_contour(skip = 1, rotate = FALSE, size = 3) +
   
-  labs(x = 'Time since P wave [seconds]',
+  labs(x = 'Time since P wave [s]',
        y = 'Respiratory cycle',
        subtitle = 'Contour height = partial CVP') +
-  ggtitle('**e.1** - Interaction between cycles') +
+  ggtitle('Interaction between cycles') +
   scale_fill_distiller(palette = "RdBu", 
                        type = "div") +
-  scale_y_continuous(labels = scales::percent) +
-  scale_linetype_ventstate
+  scale_y_continuous(labels = scales::percent) 
 
 
 time_smooth_plot <- ggplot(time_smooth, aes(time_s, est)) +
-  geom_ci_ribbon +  
   geom_line() +
   labs(x = 'Time [s]',
-       y = 'Partial CVP [mmHg]') + 
-  ggtitle('**d** - Trend over time') +
+       y = 'Partial CVP') + 
+  ggtitle('Trend over time') +
   scale_y_continuous(limits = c(intercept - 2, intercept + 2))
 
-#lattice::wireframe(est ~ qrs_rel_index * insp_rel_index, data = interact_smooth)
 
 shape_scale <- scale_shape_manual(values = c(3, 17), 
                                   limits = c('P wave (ECG)', 
@@ -179,15 +159,15 @@ shape_scale <- scale_shape_manual(values = c(3, 17),
 
 model_cvp_plot <- (cardiac_smooth_plot + insp_smooth_plot) / 
   (interact_smooth_plot + time_smooth_plot) +
-  plot_layout(heights = c(1,1)) & 
+  plot_layout(heights = c(1,1.5)) & 
   shape_scale
 # model_cvp_plot
 
 predict_cvp_plot <- ggplot(cvp_demo_60_p %>% first_20_s(), aes(time_s, CVP, colour = 'Observed')) +
   geom_line() +
   geom_line(aes(y = CVP_predict, colour = 'Predicted'), linetype = "31") +
-  labs(title = '**f** - Residuals (observed CVP - predicted CVP)',
-       y='CVP [mmHg]', x = 'Time [s]', colour = '') +
+  labs(title = 'Residuals (observed CVP - predicted CVP)',
+       y='CVP', x = 'Time [s]', colour = '') +
   scale_colour_manual(values = c(darkblue, darkred, orange)) +
   guides(color = guide_legend(override.aes = list(linetype = c(1, 2, 1)) ) ) +
   
@@ -201,9 +181,11 @@ predict_cvp_plot <- ggplot(cvp_demo_60_p %>% first_20_s(), aes(time_s, CVP, colo
 predict_resid_cvp_plot_compact <- predict_cvp_plot + 
   geom_line(aes(y = CVP_res, color = "Residual")) +
   guides(color = guide_legend(override.aes = list(linetype = c(1, 2, 1)) ) ) +
-  ylim(c(-2,17))
+  ylim(c(-2,17)) +
+  theme(legend.position = c(0.78, 1.15), legend.direction = "horizontal")
 
 combined_gam_cvp_plot_simple <- cvp_plot + model_cvp_plot + predict_resid_cvp_plot_compact + 
-  plot_layout(ncol = 1, heights = c(1, 5, 1.5), tag_level = 'new') & shape_scale
+  plot_annotation(tag_levels = "a") +
+  plot_layout(ncol = 1, heights = c(1, 5, 1.5)) & shape_scale
 
 save_plot("methods-GAM-cvp", combined_gam_cvp_plot_simple, width = 18, height = 18)
