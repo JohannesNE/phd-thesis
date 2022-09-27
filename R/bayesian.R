c1a70ecf3dd756b3dfbe5aa437bdcf829c011673
Johannes Enevoldsen <johannes.ne@gmail.com>
2022-09-27 11:50:58 +0200

    two columns

--- R/bayesian.R
+++ R/bayesian.R
@@ -30,7 +30,8 @@ get_post <- function(d) {
   post
 }
 
-make_panel <- function(n, y_max_mu = NA, y_max_sigma = NA, hide_y = FALSE) {
+make_panel <- function(n, y_max_mu = NA, y_max_sigma = NA, 
+                       hide_y = TRUE, hide_x = TRUE) {
   d <- heights |> filter(id <= n)
   
   post_n <- get_post(d)
@@ -43,12 +44,16 @@ make_panel <- function(n, y_max_mu = NA, y_max_sigma = NA, hide_y = FALSE) {
            ci_high = last(mu[cumsum(prob) < 0.975])
            )
   
-  y_theme <- if(hide_y) {
-    theme(axis.title.y = element_blank(),
-          axis.text.y = element_blank(),
-          plot.margin = margin(t = 0, r = 0, l = 20, b = 10))
+  lab_y <- if(hide_y) {
+    ylab("")
   } else{
-    theme(plot.margin = margin(t = 0, r = 0, l = 0, b = 10))
+    NULL
+  }
+  
+  lab_x <- if(hide_x) {
+    xlab("")
+  } else{
+    NULL
   }
   
   post_sigma <- post_n |> 
@@ -62,53 +67,54 @@ make_panel <- function(n, y_max_mu = NA, y_max_sigma = NA, hide_y = FALSE) {
   
   plot_mu <- ggplot(post_mu, aes(mu, dens)) +
     geom_vline(xintercept = mean_height, 
-               color = darkred, size = 0.7) +
+               color = orange, size = 0.7) +
     geom_line(linetype = densline) +
     geom_area(data = ~filter(.x, mu > ci_low, mu < ci_high),
               fill = darkred, alpha = 0.3) +
     geom_rug(aes(x = height), inherit.aes = FALSE, data = d,
              length = unit(0.1, "npc")) +
-    scale_y_continuous(expand = expansion(mult = c(0.2, 0.05)), 
-                       breaks = c(0, y_max_mu)
+    scale_y_continuous(breaks = c(0, y_max_mu),
+                       expand = expansion(mult = c(0.2, 0))
                        ) +
     coord_cartesian(clip="off", ylim = c(0, y_max_mu)) +
     labs(x = "μ [cm]", y = "Density",
-         subtitle = ifelse(n==0, 
+         title = ifelse(n==0, 
                         "n = 0 (prior)", 
                         glue::glue("n = {n}")
          )) +
-    y_theme
+    lab_y +
+    lab_x
   
   plot_sigma <- ggplot(post_sigma, aes(sigma, dens)) +
     geom_vline(xintercept = sd_height,
-               color = darkblue, size = 0.7) +
+               color = orange, size = 0.7) +
     geom_line(linetype = densline, color = darkblue) +
     geom_area(data = ~filter(.x, sigma > ci_low, sigma < ci_high),
               fill = darkblue, alpha = 0.3) +
-    scale_y_continuous(breaks = c(0, y_max_sigma)) +
+    scale_y_continuous(breaks = c(0, y_max_sigma),
+                       expand = expansion(mult = c(0.2, 0))) +
     coord_cartesian(clip="off", ylim = c(0, y_max_sigma)) +
-    labs(x = "σ [cm]", y = "Density") +
-    y_theme
+    labs(x = "σ [cm]", y = "") +
+    lab_y +
+    lab_x
   
-  plot_mu / plot_sigma 
+  plot_mu | plot_sigma 
 }
 
 
 make_panel(1)
 
 bayes_update_plot <- 
-  (make_panel(0, y_max_mu = 0.05, y_max_sigma = 0.05) |
-  make_panel(1, y_max_mu = 0.05, y_max_sigma = 0.05, hide_y = TRUE) |
-  make_panel(2, y_max_mu = 0.05, y_max_sigma = 0.05, hide_y = TRUE)) /
-  plot_spacer()/
-  (make_panel(8, y_max_mu = 0.25, y_max_sigma = 0.4) |
-  make_panel(20, y_max_mu = 0.25, y_max_sigma = 0.4, hide_y = TRUE)|
-  make_panel(100, y_max_mu = 0.25, y_max_sigma = 0.4, hide_y = TRUE)) +
-  plot_layout(heights = c(1, 0.1,1)) 
+  make_panel(0, hide_x = FALSE, y_max_mu = 0.03, y_max_sigma = 0.05) /
+  make_panel(1, y_max_mu = 0.05, y_max_sigma = 0.05) /
+  make_panel(2, y_max_mu = 0.05, y_max_sigma = 0.05) /
+  make_panel(5, hide_y = FALSE, y_max_mu = 0.1, y_max_sigma = 0.1) /
+  make_panel(20, y_max_mu = 0.2, y_max_sigma = 0.3)/
+  make_panel(100, hide_x = FALSE, y_max_mu = 0.4, y_max_sigma = 0.6)
 
 
 save_plot("method-bayes-update", bayes_update_plot,
-          width = 18, height = 10,
+          width = 12, height = 15,
           scale = 1.2)
 
 make_2d_panel <- function(n) {
